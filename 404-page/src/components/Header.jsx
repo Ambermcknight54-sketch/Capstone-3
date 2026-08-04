b4cc71cf2b3addad2019b16e3be8fb04f3b0fcb8
Amber <ambermcknight54@yahoo.com>
2026-08-04 10:09:33 -0500

    deleted a copy

--- 404-page/src/components/Header.jsx
+++
@@ -1,7 +0,0 @@
-export function Header() {
-  return (
-    <Header>
-      <h1> Redirect to the Home Page with a 404 Page </h1>
-    </Header>
-  );
-}
--- conditional-render/src/components/Header.jsx
+++
@@ -1,7 +0,0 @@
-export function Header() {
-  return (
-    <header>
-      <h1> Conditionally Hide or Show React Components </h1>
-    </header>
-  );
-}
